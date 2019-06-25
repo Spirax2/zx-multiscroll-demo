c95af8950c1e7f3c9a48ac30a44ef7cfbfe558bd
Spirax <52180911+Spirax2@users.noreply.github.com>
2019-06-25 12:31:51 +0200

    Update Engine.asm some optimizations by Spirax

--- engine.asm
+++ engine.asm
@@ -1,3 +1,6 @@
+;98% Main code by Javier Peña
+;2% optimizations by Spirax
+
 SCREEN_STILE_WIDTH EQU 13
 STARTX_SCREEN EQU 32
 STARTY_SCREEN EQU 8
@@ -7,12 +10,6 @@ prerotate_ntiles: db 0
 
 curtile: db 0
 map_offset: db 0
-save_levelmap: dw 0
-
-savesp: dw 0
-savesp2: dw 0
-savehl: dw 0
-savebc: dw 0
 curx:   db 0
 curx_tile: db 3
 previousx: db $ff
@@ -24,21 +21,20 @@ cury_tile: db 3
 map_needs_update: db 0
 
 ; Prerotate tiles for map
+; optimized by Spirax to save some T-states :)
 
 PrerotateTiles:
-        ld a, 10
-        ld iy, tiles        ; First tile
+        ld l, 10		   ; number of tiles to rotate
+		ld de, 96		   ; counter, to add per rotation
+		ld bc, 6		   ; counter, to add per line
+        ld iy, tiles       ; First tile
         ld ix, tiles+96    ; First rotation
 Prerotate_pertile:
-        ld (prerotate_ntiles), a
-        push iy
-        push ix
-        ld a, 3             ; 3 different rotations
+        ld h, 3             ; 3 different rotations
 Prerotate_outer:
-        push af
         ld a, 16            ; 16 lines
 Prerotate_inner:
-        ld (prerotate_counter), a
+		exx					;save counters and use extra registers for rotation
         ld e, (iy+0)
         ld d, (iy+1)
         ld c, (iy+2)
@@ -46,14 +42,14 @@ Prerotate_inner:
         ld l, (iy+4)
         ld h, (iy+5)         ; We get 6 bytes for a line
         ; Rotate 2 pixels to the right
-        and a
+        and a				 ; doesn't modify A just reset carry
         rr e
         rr d
         rr c
         rr b
         rr l
         rr h
-        and a
+        and a				 ; doesn't modify A just reset carry
         rr e
         rr d
         rr c
@@ -67,22 +63,16 @@ Prerotate_inner:
         ld (ix+3), b
         ld (ix+4), l
         ld (ix+5), h
-        ld bc, 6
-        add iy, bc
-        add ix, bc      ; next line
-        ld a, (prerotate_counter)
-        dec a
-        jr nz, Prerotate_inner
-        pop af
-        dec a
+		exx 				;change registers back to counters
+        add iy, bc			;iy+06 next line
+        add ix, bc      	;ix+06 next line
+		dec a				;decrease line number
+		jr nz, Prerotate_inner
+		dec h				;decrease rotation number
         jr nz, Prerotate_outer
-        pop ix
-        pop iy
-        ld bc, 96*4       ; next tile
-        add iy, bc
-        add ix, bc
-        ld a, (prerotate_ntiles)
-        dec a
+		add iy, de			;iy+96 next tile
+		add ix, de			;ix+96 next rotation tile
+		dec l				;decrease tile number
         jr nz, Prerotate_pertile
         ret
 
@@ -92,23 +82,15 @@ Prerotate_inner:
 ;   - curx
 ;   - cur_dispx
 
-
 DrawBkg: 
 		ld de, tiletable
-		ld (savesp), sp
-        ld a, (curx)
-        and $4
-        rrca
-        rrca
-        ld c, a
-        ld a, 30
-        sub c
-        ld c, a
-        ld b, 0
-;        ld hl, 16384+32
-        ld hl, 49152+32
-        add hl, bc
-
+		ld (savesp_DrawBkg), sp
+        ld hl, 49152+32+30
+		ld a, (curx)
+		and 4               ; check for odd, even
+		jr z, SkipBkgPar	; if even do nothing
+		dec l				; if odd decrement start address
+SkipBkgPar:
 		exx
 		ld bc, $0000
 		exx
@@ -144,14 +126,14 @@ nxtmap: ld a, (de)
         ld c, a
         add a, a            ; * 4
         add a, c            ; * 6
-        ld (savehl), hl
-        ld hl, (cur_dispx)
-        ld c, a
-        ld b, 0
-        add hl, bc
-        ld c, l
-        ld b, h
-        ld hl, (savehl)
+
+		ld bc, (cur_dispx)
+		add a,c
+		ld c,a
+		ld a,b
+		adc a,0				;increase B if there was carry
+		ld b,a
+ 
         inc e
         ld a, (de)
         and a
@@ -161,7 +143,7 @@ nxt_start:
         inc e
         add iy, bc			; IY points to the first tile + displacement
         add ix, bc			; IX points to the second tile + displacement
-nxtrow: ld b, $16          ; Number of lines per stile
+nxtrow: ld b, $16           ; Number of lines per stile
         ld a, b
         and a
         jr z, nxtmap
@@ -180,32 +162,29 @@ tilerw: jp $0000           ; And go draw the next scanline
 nxtscn: ld de, $0006
         add iy, de         ; Step down one row in the tiles
         add ix, de
-        exx                ; Switch in our working registers.
-                           ; HL=Screen line, B=tile line, C=Tile row, DE=map
-        inc h              ; Down one scanline in display
+        exx                	; Switch in our working registers.
+							; HL=Screen line, B=tile line, C=Tile row, DE=map
+        inc h              	; Down one scanline in display
     	ld a,h
 		and 7
-		jr nz, nxt_nextthird		; if the low 3 bits of H are zero
+		jr z, nxt_nextchar	; if the low 3 bits of H are zero
+        djnz nxtln         	; Retrieve tile data and draw another scanline 
+        jp nxtmap
 nxt_nextchar:
         ld a, l
         add a, $20         ; Move down one screen row
         ld l, a
-        jr c, nxt_nextthird        ; If carry then we crossed into the next third
+        jr c, nxt_nextthird  ; If carry then we crossed into the next third
         ld a, h            ; Otherwise we need to subtract 8 from H
         sub $08            ; to stay in the same screen segment
         ld h, a
 nxt_nextthird:
         djnz nxtln         ; Retrieve tile data and draw another scanline
-;inseg:  dec c
-        jr nxtmap
-;second_tile:
- ;       ld b, $00
-  ;      jr nxtln
-endmap: ld sp, (savesp)
+        jp nxtmap
+
+savesp_DrawBkg: equ $+1
+endmap: ld sp, 0
         ret
-
-
-
 
 
 ; Prepare tables for next draw
@@ -219,7 +198,7 @@ PrepareMapUpdate:
         ld de, line1
         ld (map_offset), a
 
-        ld hl, levelmap
+       ld hl, levelmap
         ; Add Y offset
         ld a, (mapwidth)
         ld c, a
@@ -238,9 +217,10 @@ no_addy:
         ld c, a
         ld b, 0
         add hl, bc
+		ex de, hl	;now HL point to the LINE and DE to the levelmap+offset
         ld b, 8     ; outer counter
 prepare_outer_loop:
-        ld (save_levelmap), hl
+		ld (save_levelmap_loop), de
         ld a, $ff
         exx
         ld b, a
@@ -248,13 +228,13 @@ prepare_outer_loop:
         ld d, a
         exx
         ld c, SCREEN_STILE_WIDTH    ; inner counter
-        push de
+		push hl
 prepare_loop:
-        ld a, (hl)
+		ld a, (de)
         and a
-        jp z, prepare_tile_empty    ; not switching the tile for an empty one
+        jr z, prepare_tile_empty    ; not switching the tile for an empty one
         ex af, af'
-        ld a, (hl)
+		ld a, (de)
         rrca
         rrca
         and $3f         ; Tile number / 4 is the actual 48x16 supertile        
@@ -264,11 +244,11 @@ prepare_loop:
         jp z, prepare_tile1_same  ; the same current tile, just push
         exx
         cp b            ; is it tile1?
-        jr z, prepare_switch_tile1
+        jp z, prepare_switch_tile1
         ; if it's not, this might be the first one
         ld h, b
         inc h
-        jr nz, prepare_loop_is_it_tile2 ; so it's not the first time
+        jp nz, prepare_loop_is_it_tile2 ; so it's not the first time
         ; First tile and current tile is A
         ld b, a
         ld d, a
@@ -282,7 +262,13 @@ prepare_loop:
         add a, h
         add a, $a0      ; Tiles start in $A000
         ld (ix+1), a
-        jr prepare_tile1
+        jp prepare_tile1
+
+prepare_tile_empty:
+		ld (hl), $C5   ; write the opcode PUSH BC
+		inc l
+		jp prepare_loop_next
+		
 prepare_loop_is_it_tile2:
         ;  this could be the first time the second tile is found
         ld h, c
@@ -301,65 +287,77 @@ prepare_loop_is_it_tile2:
         add a, $a0      ; Tiles start in $A000
         ld (ix+3), a
         ld a, h         ; restore the value of A
-        jr prepare_switch_tile2
+        jp prepare_switch_tile2
 prepare_switch_tile1:
         ld d, a     ; set the current tile
         ; We are going to insert the following sequence:
-        ;         ld (savesp), sp           ED 73 xx yy
+        ;         ld ($+8), sp              ED 73 L+8 H
         ;         ld sp, iy                 FD F9
         ;         pop de                    D1
         ;         pop hl                    E1
         ;         pop af                    F1
-        ;         ld sp, (savesp)           ED 7B xx yy
+        ;         ld sp, xxyy	            31 xx yy
         exx
-        ld (savehl), hl
-        ld (savebc), bc
-        ld hl, block_iy
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ld hl, (savehl)
-        ld bc, (savebc)
-        jr prepare_tile1_same
+		ld (hl), $ED
+		inc l
+		ld (hl), $73
+		inc l
+		ld a, 8
+		add a, l
+		ld (hl),a
+		inc l
+		ld (hl),h
+		inc l
+		ld (hl), $FD 
+		inc l
+		ld (hl), $F9
+		inc l
+     	ld (hl), $D1
+		inc l
+     	ld (hl), $E1
+		inc l
+      	ld (hl), $F1
+		inc l
+	 	ld (hl), $31 		;ld sp, xxyy
+		inc l
+		inc l
+		inc l
+        jp prepare_tile1_same
 prepare_switch_tile2:
         ld d, a     ; set the current tile
         ; We are going to insert the following sequence:
-        ;         ld (savesp), sp
-        ;         ld sp, ix
-        ;         pop de
-        ;         pop hl
-        ;         pop af
-        ;         ld sp, (savesp)
+        ;         ld ($+8), sp              ED 73 L+8 H
+        ;         ld sp, ix                 DD F9
+        ;         pop de                    D1
+        ;         pop hl                    E1
+        ;         pop af                    F1
+        ;         ld sp, xxyy	            31 xx yy
         exx
-        ld (savehl), hl
-        ld (savebc), bc
-        ld hl, block_ix
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ldi
-        ld hl, (savehl)
-        ld bc, (savebc)
-        jr prepare_tile1_same
+		ld (hl), $ED
+		inc l
+		ld (hl), $73
+		inc l
+		ld a, 8
+		add a, l
+		ld (hl),a
+		inc l
+		ld (hl),h
+		inc l
+		ld (hl), $DD 
+		inc l
+		ld (hl), $F9
+		inc l
+     	ld (hl), $D1
+		inc l
+     	ld (hl), $E1
+		inc l
+      	ld (hl), $F1
+		inc l
+	 	ld (hl), $31 			;ld sp,xxyy
+		inc l
+		inc l
+		inc l
+        jp prepare_tile1_same
 prepare_tile1:
         exx
 prepare_tile1_same:
@@ -371,63 +369,33 @@ prepare_tile1_ok:
         rrca
         rrca        ; set it in bits 00xx0000
         or 11000101b    ; PUSH AF opcode is F5 (3), PUSH HL is E5 (2), PUSH DE is D5 (1), PUSH BC is C5 (0)
-        ld (de), a      ; write the opcode
-        inc de        
+		ld (hl), a		 ; write the opcode
+		inc l
 prepare_loop_next:
-        dec hl
+		dec de
         dec c
-        jr z, prepare_nextline
-        jp prepare_loop
-prepare_tile_empty:
-        ld a, $C5   ; PUSH BC
-        ld (de), a      ; write the opcode
-        inc de        
-        jr prepare_loop_next
+        jp nz, prepare_loop
 prepare_nextline:
-        ld a, $C3   ; JP XX
-        ld (de), a
-        inc de
-        ld hl, nxtscn
-        ld a, l
-        ld (de), a
-        inc de
-        ld a, h
-        ld (de), a
+        ld (hl), $C3			;JP nxtscn
+        inc l
+		ld (hl), low nxtscn
+        inc l
+		ld (hl), high nxtscn
         ld de, 8
         add ix, de              ; next line
-        pop de
-        ld hl, 64
-        add hl, de
-        ex de, hl
+        pop hl
+        ld de, 64
+        add hl, de				; now HL has the next scn LINE
         dec b
         ret z
-
-        ld hl, (save_levelmap)
-        push bc
+save_levelmap_loop: equ $+1
+		ld de, 0				;DE will be modified with the actual MAP Address to add mapwidth
         ld a, (mapwidth)
-        ld c, a
-        ld b, 0
-        add hl, bc
-        pop bc
-
-        jp prepare_outer_loop
-
-block_iy:                     ; 13 bytes
-    ld (savesp2), sp           ; ED 73 xx yy
-    ld sp, iy                 ; FD F9
-    pop de                    ; D1
-    pop hl                    ; E1
-    pop af                    ; F1
-    ld sp, (savesp2)           ; ED 7B xx yy
-
-block_ix:                     ; 13 bytes
-    ld (savesp2), sp           ; ED 73 xx yy
-    ld sp, ix                 ; DD F9
-    pop de                    ; D1
-    pop hl                    ; E1
-    pop af                    ; F1
-    ld sp, (savesp2)           ; ED 7B xx yy
-
+		add a, e
+		ld e, a
+		jp nc, prepare_outer_loop
+		inc d
+		jp prepare_outer_loop
 
 
 ; Move Camera, based on the reference created by an entity
@@ -442,26 +410,24 @@ MoveCamera:
     ld bc, 88           ; FIXME magic constant
     and a
     sbc hl, bc          ; X - 88 is where the camera should be
-    push hl
+
     ld a, l        ; to get curx we just need the low byte
     and $f
     rrca
     ld (curx), a
-    and 3
-    ld b, a
-    ld a, 3
-    sub b
-    add a, a            ; (3-a) is the actual value
-    ld hl, Multiply_by_96
-    ld c, a
-    ld b, 0
-    add hl, bc		; HL points to the value in the array
-    ld c, (hl)
-    inc hl
-    ld b, (hl)		; BC = 96 * xcounter & 3
-    ld (cur_dispx), bc
-    pop hl          ; now, divide by 16 to get curx_tile
-    srl h
+	cpl
+    and 3				; (3-a) is the actual value
+    add a, a            ; a * 2
+    ld bc, Multiply_by_96 ;Note Multiply_by_96 is aligned to be on the same high Byte so we only operate with C and also we can do inc C instead inc BC
+    add a, c
+	ld c, a
+	ld a, (bc)
+    ld (cur_dispx), a
+	inc c
+	ld a, (bc)		
+    ld (cur_dispx+1), a
+
+    srl h 			; now, divide HL by 16 to get curx_tile
     rr l
     srl h
     rr l
@@ -491,31 +457,28 @@ MoveCamera:
     ld a, l 
     ld (cury_tile), a
     ; Finally, check if we need to recalculate anything from the pop stuff
-    ld a, (previousx)
-    ld b, a
+    ld hl, previousx
     ld a, (curx_tile)
-    cp b       
+    cp (hl)       
     jr z, MoveCamera_noxchange
-    ld (previousx), a
+    ld (hl), a
     ld a, 1
     ld (map_needs_update), a
 MoveCamera_noxchange:
-    ld a, (previousy)
-    ld b, a
+    inc hl		; now HL points to previousy
     ld a, (cury_tile)
-    cp b
+    cp (hl)
     jr z, MoveCamera_noychange
-    ld (previousy), a
+    ld (hl), a
     ld a, 1
     ld (map_needs_update), a
 MoveCamera_noychange:
     ; If the camera moved in Y, we need to adjust the tables too
-    ld a, (previous_ysub)
-    ld b, a
+    inc hl 				;now HL points to previous_ysub
     ld a, (cury)
-    cp b
+    cp (hl)
     jr z, MoveCamera_end
-    ld (previous_ysub), a
+    ld (hl), a
     ld (tiletable+8*7+6), a
     sub 16
     neg
@@ -525,5 +488,4 @@ MoveCamera_end:
     and a
     ret z
     ld a, (curx_tile)
-    call PrepareMapUpdate
-    ret
+    jp PrepareMapUpdate
